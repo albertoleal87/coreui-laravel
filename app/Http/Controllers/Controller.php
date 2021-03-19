eb3515010635740d3b05dc06663172dc8556a996
Alberto Leal <betoleal87@gmail.com>
2021-03-18 22:51:42 -0600

    Print Route Request in log

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,7 +7,30 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+use Auth;
+use Route;
+use Request;
+use Log;
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    	public function __construct(){
+		
+			$this->middleware(function ($request, $next) {
+
+				if(Auth::user()){
+					$track = [
+						'CurrentRouteName' => Route::currentRouteName(),
+						'RequestUrl' => Request::url(),
+						'User'=> Auth::user()->email,
+					];
+
+					Log::info($track);
+				}
+
+				return $next($request);
+			});		
+	}
 }
